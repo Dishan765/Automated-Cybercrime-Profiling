e4b1e1e9de2c64cd8f32a364b7c5fda0dabcd367
dishan <you@example.com>
2021-03-12 17:15:27 +0400

    Initial Commit;Scrape comments from Facebook

---
+++ Preprocessing.py
@@ -0,0 +1,50 @@
+import demoji
+import pandas as pd
+
+df = pd.read_csv('Dataset.csv')
+df.head()
+
+def lower_case():
+  
+
+#Cleaning
+def remove_emogies():
+  pass
+
+#Cleaning
+def remove_punctuations():
+  pass
+
+#Cleaning
+def remove_urls():
+  pass
+
+#Remove unique words
+def remove_rareWords():
+  pass
+
+#Frequent words are not removed
+#since it is dealt by TF-IDF
+
+#
+def remove_stopwords():
+  pass
+
+
+def tokenize(text):
+  pass
+
+#Lemmatization
+def lemmatize_words(text):
+  pass
+
+#Normalization
+def correct_spellings(text):
+  pass
+
+
+def main():
+  pass
+
+if __name__ == "__main__":
+    main()
